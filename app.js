872535f4ea042ec1102cec049a29e43360030c2e
fjmarson <fjmarson@gmail.com>
2024-02-08 12:55:28 -0300

    Actualiza el app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
 
     if (isNaN(numeroDeUsuario) || numeroDeUsuario < 1 || numeroDeUsuario > numeroMaximo) {
-        asignarTextoElemento('p', 'Ingresa un número válido dentro del rango.');
+        asignarTextoElemento('p', 'Dale Andrés!!! Ingresa un número válido dentro del rango.');
         return;
     }
     
